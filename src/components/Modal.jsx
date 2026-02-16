6bdad2b9063f9623a6afa406d57b19f03bc39c4b
uwem4life <wemoren@gmail.com>
2026-02-17 00:44:43 +0100

    integrated modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,21 +1,33 @@
 import React from "react";
+import Metamask from "../assets/images/dogface.png";
+import WalletConnect from "../assets/images/bluetooth.png";
 import { X } from "lucide-react";
 const Modal = ({ open, setOpenModal }) => {
   if (!open) return null;
   return (
     <div className="modal-wrapper" onClick={() => setOpenModal(false)}>
       <div className="madal-content" onClick={(e) => e.stopPropagation()}>
-        <div className="modal-heading">
-          <h3>Connect Wallet</h3>
-          <X onClick={() => setOpenModal(false)} className="close" />
-          <hr />
+        <h3>Connect Wallet</h3>
+        <X onClick={() => setOpenModal(false)} className="close" />
 
-          <div className="wallets">
-            <p>Choose your prefered wa</p>
-            <div className="metamask">
-              <img src="../assets/images/metamask.png" alt="" />
-              <h6>Metamask</h6>
-            </div>
+        <hr />
+
+        <div className="wallets">
+          <p>Choose your prefered wallet</p>
+          <div className="metamask-cont">
+            <img src={Metamask} alt="metamask" className="metamask" />
+            <h6>Metamask</h6>
+            <span className="forward">{">"}</span>
+          </div>
+
+          <div className="walletconnect-cont">
+            <img
+              src={WalletConnect}
+              alt="walletconnect"
+              className="walletconnect"
+            />
+            <h6>WalletConnect</h6>
+            <span className="forward">{">"}</span>
           </div>
         </div>
       </div>
